ec85ca1f3cc22759e9a232314d552a4c84678d73
Kyle Finley <kyle.finley@gmail.com>
2012-01-09 23:28:24 -0600

    Once again attempting to get read the docs to recognize on_rtd

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 import os
 import sys
 
-on_rtd = os.environ.get('READTHEDOCS', None)
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -132,7 +132,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
+html_theme = 'default'
 
 if on_rtd:
     html_theme = 'default'
