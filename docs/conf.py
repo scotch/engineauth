6b60786206b53be1b92bdba0730b8d5a27ae4a28
Kyle Finley <kyle.finley@gmail.com>
2012-01-10 09:47:24 -0600

    converted back to 'sphinx-bootstrap'

--- docs/conf.py
+++ docs/conf.py
@@ -139,7 +139,7 @@ if on_rtd:
 else:
     # The theme to use for HTML and HTML Help pages.  See the documentation for
     # a list of builtin themes.
-    html_theme =  os.environ
+    html_theme = 'sphinx-bootstrap'
 
     # Theme options are theme-specific and customize the look and feel of a theme
     # further.  For a list of options available for each theme, see the
