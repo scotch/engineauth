b84be3c93a959811003fbe6da6b1f64f883b728d
Kyle Finley <kyle.finley@gmail.com>
2012-01-12 11:41:09 -0600

    Added more test of middleware

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -7,7 +7,6 @@ import webapp2
 __author__ = 'kyle.finley@gmail.com (Kyle Finley)'
 
 
-
 app = AuthMiddleware(webapp2.WSGIApplication())
 
 class TestAuthMiddleware(test_base.BaseTestCase):
@@ -110,3 +109,134 @@ class TestAuthMiddleware(test_base.BaseTestCase):
         s1 = models.Session.query().get()
         self.assertEqual(s1.key.id(), '1')
 
+    def test__load_user(self):
+        user = models.User.create_user('test:12345')
+        req = EngineAuthRequest.blank('/auth/google')
+        req._load_session()
+        req.session.user_id = user.get_id()
+        req._load_user()
+        self.assertEqual(user, req.user)
+
+    def test__load_user_by_profile(self):
+        # No existing User no logged in User
+        auth_id = 'test:12345'
+        user_info = {
+            'auth_id': auth_id,
+            'info': {},
+        }
+        # create profile
+        p = models.UserProfile.get_or_create(auth_id, user_info)
+        req = EngineAuthRequest.blank('/auth/google')
+        req._load_session()
+        req._load_user()
+
+        # User Count before
+        user_count = models.User.query().count()
+        self.assertEqual(user_count, 0)
+
+        req.load_user_by_profile(p)
+
+        # User Count after
+        user_count = models.User.query().count()
+        self.assertEqual(user_count, 1)
+
+        user = models.User.query().get()
+        self.assertTrue(p.key.id() in user.auth_ids)
+
+        # Yes existing User no logged in User
+        req = EngineAuthRequest.blank('/auth/google')
+        req._load_session()
+        req._load_user()
+
+        req.load_user_by_profile(p)
+
+        # Test to no new User was created
+        user_count = models.User.query().count()
+        self.assertEqual(user_count, 1)
+
+        # Yes existing User yes logged in User new Profile
+        auth_id = 'test:abc'
+        user_info = {
+            'auth_id': auth_id,
+            'info': {},
+            }
+        # create profile
+        p1 = models.UserProfile.get_or_create(auth_id, user_info)
+        req.load_user_by_profile(p1)
+
+        # Test to no new User was created
+        user_count = models.User.query().count()
+        self.assertEqual(user_count, 1)
+
+    def test_add_message(self):
+        req = EngineAuthRequest.blank('/auth/google')
+        req._load_session()
+
+        msgs = req.get_messages()
+        self.assertEquals(msgs, None)
+
+        req.add_message('TEST MESSAGE')
+        msgs = req.get_messages()
+        self.assertEquals(msgs, [{'level': None, 'message':'TEST MESSAGE' }])
+
+        # Get again should be none.
+        msgs = req.get_messages()
+        self.assertEquals(msgs, None)
+
+        # add message with level error
+        req.add_message('TEST1', 'error')
+        # add another message with level error
+        req.add_message('TEST2', 'success')
+
+        msgs = req.get_messages()
+        self.assertEquals(msgs, [
+                {'level': 'error', 'message':'TEST1' },
+                {'level': 'success', 'message':'TEST2' },
+        ])
+        # Get again should be none.
+        msgs = req.get_messages()
+        self.assertEquals(msgs, None)
+
+        # Test with different key.
+        # add message with level error
+        req.add_message('TEST1', 'error')
+        # add another message with level error
+        req.add_message('TEST2', 'success', '_mykey')
+
+        msgs = req.get_messages()
+        self.assertEquals(msgs, [
+                {'level': 'error', 'message':'TEST1' },
+        ])
+        msgs_key = req.get_messages('_mykey')
+        self.assertEquals(msgs_key, [
+                {'level': 'success', 'message':'TEST2' },
+        ])
+        # Get again should be none.
+        msgs = req.get_messages()
+        self.assertEquals(msgs, None)
+        msgs_key = req.get_messages()
+        self.assertEquals(msgs_key, None)
+
+    def test_set_redirect_uri(self):
+        # test without next uri
+        req = EngineAuthRequest.blank('/auth/google')
+        req._load_session()
+        req.set_redirect_uri()
+        req.ea_config = {'success_uri': '/callback'}
+        redirect_uri = req.get_redirect_uri()
+        self.assertEqual(redirect_uri, '/callback')
+
+        # test with out next uri
+        req = EngineAuthRequest.blank('/auth/google?next=/newcallback')
+        req._load_session()
+        req.set_redirect_uri()
+        req.ea_config = {'success_uri': '/callback'}
+        redirect_uri = req.get_redirect_uri()
+        self.assertEqual(redirect_uri, '/newcallback')
+
+        req = EngineAuthRequest.blank('/auth/google?next=/newcallback&a=121&123=a')
+        req._load_session()
+        req.set_redirect_uri()
+        req.ea_config = {'success_uri': '/callback'}
+        redirect_uri = req.get_redirect_uri()
+        self.assertEqual(redirect_uri, '/newcallback')
