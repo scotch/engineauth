e53c5567878895e338d9f714fcc5bbd59e87b686
Kyle Finley <kyle.finley@gmail.com>
2012-09-26 11:42:57 -0500

    docs: updated symbolic link

--- docs/conf.py
+++ docs/conf.py
@@ -214,7 +214,7 @@ html_show_sphinx = True
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-#html_use_opensearch = ''
+html_use_opensearch = ''
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
 #html_file_suffix = None
