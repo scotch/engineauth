6d528a461c5ccc9cbaf54814b0d546c90cd65173
Kyle Finley <kyle.finley@gmail.com>
2013-05-06 14:48:49 -0500

    doc: added not about the purpose of the _get_kind() method

--- example/models.py
+++ example/models.py
@@ -3,4 +3,7 @@ from engineauth import models
 class CustomUser(models.User):
     @classmethod
     def _get_kind(cls):
+        # Override the datastore entity name.
+        # The string that is returned here will be used to name the entity
+        # group in the datastore
         return 'EAUser'
