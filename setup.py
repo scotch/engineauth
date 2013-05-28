f1d50f69c7bca349c63b488b2a71fae2f23831c4
Kyle Finley <git@kylefinley.com>
2013-05-28 01:09:36 -0500

    bumped version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name = 'EngineAuth',
-    version = '0.1',
+    version = '0.2.0',
     license = 'Apache Software License',
     url = 'http://engineauth.appspot.com',
     description = "Google App Engine: Multi-Provider Authentication Simplified.",
