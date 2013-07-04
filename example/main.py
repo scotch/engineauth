4325122426ab99bc4c44c2e8c7e404d4d1a4cb69
Kyle Finley <git@kylefinley.com>
2013-07-04 12:08:25 -0500

    cleanup: remove unused code
    fixes: #16

--- example/main.py
+++ example/main.py
@@ -23,10 +23,6 @@ config = {
     'webapp2_extras.sessions': {
         'secret_key': 'wIDjEesObzp5nonpRHDzSp40aba7STuqC6ZRY'
     },
-    'webapp2_extras.auth': {
-        #        'user_model': 'models.User',
-        'user_attributes': ['displayName', 'email'],
-        },
     'webapp2_extras.jinja2': {
         'filters': {
             'do_pprint': do_pprint,
